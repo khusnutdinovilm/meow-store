f701861cd787d214cd73b74f8daad8f5dee9136d
Ilmir Khusnutdinov <khusnutdinov.ilmir.w7@gmail.com>
2025-01-03 23:34:59 +0500

    Разработка footer
    - сверстал компонент footer
    - добавил временный блок, вместо ui-элементов

--- src/app.vue
+++ src/app.vue
@@ -2,9 +2,12 @@
   <app-header />
 
   <router-view />
+
+  <app-footer />
 </template>
 
 <script setup lang="ts">
+import AppFooter from "common/app-footer";
 import AppHeader from "common/app-header";
 </script>
 
